a3dbddba40a9bacf860e88fbb24cd85ffdf3f177
teamgroup57 <fatima32munshi@gmail.com>
2024-05-10 02:15:18 +0900

    log.info at signin()

--- src/main/java/com/tjoeun/shop/controller/MemberController.java
+++ src/main/java/com/tjoeun/shop/controller/MemberController.java
@@ -3,11 +3,15 @@ package com.tjoeun.shop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class MemberController {
 
 	@GetMapping("/member/signin")
 	public String singin() {
+		log.info(">>>>>>>>>>>>>>>> signin");
 		return "singin";
 	}
 
