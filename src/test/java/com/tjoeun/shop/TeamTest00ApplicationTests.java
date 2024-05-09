7cd360808599a14e843122d74146a17283139c4b
lew <bit4math@gmail.com>
2024-05-10 01:49:35 +0900

    first commit

---
+++ src/test/java/com/tjoeun/shop/TeamTest00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjoeun.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTest00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
